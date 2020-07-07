d72473b6f82092ce61d61c751471dd884af51151
p.franek <p.franek@kendaxa.com>
2020-07-07 23:16:04 +0200

    add functions for tesseract ocr

--- pdf_tools/converter.py
+++ pdf_tools/converter.py
@@ -1,11 +1,16 @@
-"""Various basic tools for conversions between pdf's, text, images and words and word indices.
-
-The most basic conversion function are just wrappers around the Poppler utils `pdftotext` and `pdftoppm`.
-"""
+"""Various basic tools for conversions between pdf's, text, images and words and word indices."""
+import logging
+import subprocess
+import shutil
+from pathlib import Path
+from tempfile import mkdtemp
 from typing import Union, List, Tuple, Dict
 
 import numpy as np
 import pdf2image
+import pytesseract
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.utils import ImageReader
 from PIL import Image
 
 from .rectangle import Rectangle
@@ -74,6 +79,94 @@ def pdf_box_to_image_box(pdf_box: Rectangle,
 def save_images_to_pdf(images: List[Image.Image], output_pdf: str) -> None:
     """Save a list of images as a vanilla image-pdf (no text content), each image on one page."""
     images[0].save(output_pdf, "PDF", save_all=True, append_images=images[1:])
+
+
+def ocr_one_image(im: Image.Image,
+                  lang: str = "eng",
+                  config: str = "--psm 12 --oem 3") -> List[Dict]:
+    """Compute a dictionary with detected words and bounding boxes.
+
+    :param im: input image
+    :param lang: language code
+    :param config: tesseract configuration
+    :return: list of dictionaries of type {"word": word, "bb": bounding box of the word}
+    """
+    d = pytesseract.image_to_data(
+        im, output_type=pytesseract.Output.DICT, lang=lang, config=config)
+    result = []
+    for i in range(len(d["level"])):
+        word = d["text"][i]
+        if word.strip():
+            left, top, width, height = d["left"][i], d["top"][i], d["width"][i], d["height"][i]
+            result.append({
+                "word": word,
+                "bb": Rectangle(left, top, left + width, top + height)
+            })
+    return result
+
+
+def image_to_one_page_ocred_pdf(im: Image.Image,
+                                pdf_path: str,
+                                ocr_text: List[Dict],
+                                output_pdf_width: int,
+                                font_name: str = "Helvetica") -> None:
+    """Convert the image into a pdf with added textual layer and store it to disc.
+
+    Run tesseract OCR and add invisible textual content so that the pdf is searchable / clickable.
+    :param im: input image
+    :param pdf_path: path to output pdf
+    :param ocr_text: information about words and their bounding boxes
+    :param output_pdf_width: with of output pdf page
+    :param font_name
+    """
+    rescale = output_pdf_width / im.size[0]
+    if rescale > 1:
+        logging.warning("making the pdf page larger than the image (consider doing ocr on larger im)")
+    output_pdf_height = int(im.size[1] * rescale)
+    new_pdf = Canvas(pdf_path, pagesize=(output_pdf_width, output_pdf_height))
+
+    im_resized = im.resize((output_pdf_width, output_pdf_height))
+    new_pdf.drawImage(
+        ImageReader(im_resized),
+        0, 0, width=output_pdf_width, height=output_pdf_height)
+
+    for word_and_position in ocr_text:
+        word = word_and_position["word"]
+        bb = word_and_position["bb"].rescale(multiply_width_by=rescale, multiply_height_by=rescale)
+
+        text = new_pdf.beginText()
+        text.setFont(font_name, bb.height)
+        text.setTextRenderMode(3)  # invisible
+        text.setTextOrigin(bb.x_min, output_pdf_height - bb.y_max)  # bottom-left corner
+        text.setHorizScale(100 * bb.width / new_pdf.stringWidth(word, "Helvetica", bb.height))
+        text.textLine(word)
+        new_pdf.drawText(text)
+
+    new_pdf.save()
+
+
+def convert_image_list_to_searchable_pdf(images: List[Image.Image],
+                                         pdf_path: str,
+                                         output_pdf_width: int,
+                                         tesseract_lang: str = "eng",
+                                         tesseract_config: str = "--psm 12 --oem 3"
+                                         ):
+    """Create a pdf from images with digital content comming from pytesseract."""
+    td, tmp_paths = mkdtemp(), []
+    for page_idx, im in enumerate(images):
+        current_tmp_path = str(Path(td) / f"{page_idx}.pdf")
+        tmp_paths.append(current_tmp_path)
+        image_to_one_page_ocred_pdf(
+            im,
+            pdf_path=current_tmp_path,
+            ocr_text = ocr_one_image(im, tesseract_lang, tesseract_config),
+            output_pdf_width=output_pdf_width)
+
+    # combine single-page pdfs into one
+    subprocess.run([
+        "pdfunite", *tmp_paths, pdf_path
+    ])
+    shutil.rmtree(td)
 
 
 def get_indices_of_words(words: List[str], char_span: Tuple[int, int]) -> Dict:
