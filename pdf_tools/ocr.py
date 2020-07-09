56242c6492c4ca7f3ec2e29eeaa29dbb57bd023f
p.franek <p.franek@kendaxa.com>
2020-07-09 23:53:42 +0200

    minor style improvments

--- pdf_tools/ocr.py
+++ pdf_tools/ocr.py
@@ -14,30 +14,29 @@ class Scanner:
 
     @classmethod
     def ocr_one_image(cls,
-                      im: Image.Image,
+                      img: Image.Image,
                       lang: str = "eng",
                       config: str = "--psm 1 --oem 3") -> List[Dict]:
         """Compute a dictionary with detected words and bounding boxes.
 
-        :param im: input image
+        :param img: input image
         :param lang: language code
         :param config: tesseract configuration
         :return: list of dictionaries of type {"word": word, "bb": bounding box of the word, relative to page size}
         """
         d = pytesseract.image_to_data(
-            im, output_type=pytesseract.Output.DICT, lang=lang, config=config)
+            img, output_type=pytesseract.Output.DICT, lang=lang, config=config)
         result = []
-        for i in range(len(d["level"])):
-            word = d["text"][i]
+        for i, word in enumerate(d["text"]):
             if word.strip():
                 left, top, width, height = d["left"][i], d["top"][i], d["width"][i], d["height"][i]
                 result.append({
                     "word": word,
-                    "bb": Rectangle(left, top, left + width, top + height).relative_to_size(
-                        width=im.size[0],
-                        height=im.size[1]
-                    )
-                })
+                    "bb": Rectangle(
+                        x_min=left,
+                        y_min=top,
+                        x_max=left + width,
+                        y_max=top + height).relative_to_size(width=img.size[0], height=img.size[1])})
         return result
 
     @classmethod
